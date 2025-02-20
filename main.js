edd7290e01b5c9f495c97d4ad91454543461f3f7
Daisy Nardella <daisy50@live.it>
2025-02-20 16:43:37 +0100

    terminatoCicli3

---
+++ main.js
@@ -0,0 +1,14 @@
+let choosebeverage = +prompt('Cosa vuoi bere? \n 1- acqua \n 2- coca-cola \n 3-birra');
+
+do {
+    if(choosebeverage == 1){
+        console.log("E’ stata selezionata l’acqua");
+    }else if(choosebeverage == 2){
+        console.log("E’ stata selezionata la coca-cola");
+    }else if(choosebeverage == 3){
+        console.log("E’ stata selezionata la birra");
+    }else{
+        console.log("Scelta non valida");
+    }   
+} while (choosebeverage < 1 && choosebeverage >3 )
+
